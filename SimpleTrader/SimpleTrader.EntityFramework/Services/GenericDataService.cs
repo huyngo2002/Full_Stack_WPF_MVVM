f67466329be6f391b4728b0b86839353b1ece434
Nguyễn Huy Ngọ <huyngu20@gmail.com>
2023-07-14 22:28:04 +0700

    create

---
+++ SimpleTrader/SimpleTrader.EntityFramework/Services/GenericDataService.cs
@@ -0,0 +1,49 @@
+﻿using SimpleTrader.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.EntityFramework.Services
+{
+    public class GenericDataService<T> : IDataService<T> where T : class
+    {
+        private readonly SimpleTraderDbContextFactory _contextFactory;
+
+        public GenericDataService(SimpleTraderDbContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public async Task<T> Create(T entity)
+        {
+            using (SimpleTraderDbContext context = _contextFactory.CreateDbContext())
+            {
+                var createdEntity = await context.Set<T>().AddAsync(entity);
+                await context.SaveChangesAsync();
+                return createdEntity.Entity; 
+            }
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> Value(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SimpleTrader/SimpleTrader.EntityFramework/SimpleTraderDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.EntityFramework
+{
+    public class SimpleTraderDbContextFactory : IDesignTimeDbContextFactory<SimpleTraderDbContext>
+    {
+        public SimpleTraderDbContext CreateDbContext(string[] arg = null)
+        {
+            var options = new DbContextOptionsBuilder<SimpleTraderDbContext>();
+            options.UseSqlServer("Server=.\\SQLEXPRESS;Database=SimpleTraderDB;Trusted_Connection=True;");
+            return new SimpleTraderDbContext(options.Options);
+        }
+    }
+}
